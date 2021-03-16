ce1ca69336fba72deffd4a5465a5e02f9c354f8f
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-16 13:33:14 -0300

    Adicionei import VideoGamesDemoStoreApp e adicionei acao clicar

--- src/test/java/com/demo/store/tasks/ProductDemoStoreTask.java
+++ src/test/java/com/demo/store/tasks/ProductDemoStoreTask.java
@@ -1,19 +1,24 @@
 package com.demo.store.tasks;
 
 import com.demo.store.appobjects.IndexDemoStoreApp;
+import com.demo.store.appobjects.VideoGamesDemoStoreApp;
 import org.openqa.selenium.WebDriver;
 
 public class ProductDemoStoreTask {
 
     private final WebDriver driver;
     private final IndexDemoStoreApp indexDemoStoreApp;
+    private final VideoGamesDemoStoreApp videoGamesDemoStoreApp;
 
     public ProductDemoStoreTask(WebDriver driver) {
         this.driver = driver;
         indexDemoStoreApp = new IndexDemoStoreApp(driver);
+        videoGamesDemoStoreApp = new VideoGamesDemoStoreApp(driver);
     }
 
     public void escolheDoisJogos(){
         indexDemoStoreApp.abaVideoGamesButton().click();
+        videoGamesDemoStoreApp.batmanArkhamCityX360CEButton().click();
+
     }
 }
