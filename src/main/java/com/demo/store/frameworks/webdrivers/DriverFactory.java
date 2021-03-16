f47abb5fdcbeb96e82e99611785528254deef24a
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-16 11:31:13 -0300

    Criei class, adicionei switch para escolher navegadores

---
+++ src/main/java/com/demo/store/frameworks/webdrivers/DriverFactory.java
@@ -0,0 +1,30 @@
+package com.demo.store.frameworks.webdrivers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public WebDriver driver;
+
+    public WebDriver createBrowser ( Browser tipos) {
+        switch (tipos) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+            default:
+                String message = "DriverFactory.createBrowser() recebeu um argumento inavalido.";
+                throw new IllegalArgumentException(message);
+        }
+    }
+
+}
