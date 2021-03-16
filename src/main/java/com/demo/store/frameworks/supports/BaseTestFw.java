ffaff82c992b943c8a2bf4d0b8378571031b71a4
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-16 16:16:39 -0300

    Coloquei metodos configurar e fecha para report, janela maxima

--- src/main/java/com/demo/store/frameworks/supports/BaseTestFw.java
+++ src/main/java/com/demo/store/frameworks/supports/BaseTestFw.java
@@ -1,5 +1,6 @@
 package com.demo.store.frameworks.supports;
 
+import com.demo.store.frameworks.tools.Report;
 import com.demo.store.frameworks.webdrivers.Browser;
 import com.demo.store.frameworks.webdrivers.DriverFactory;
 import com.demo.store.frameworks.webdrivers.DriverManager;
@@ -10,13 +11,16 @@ public class BaseTestFw extends DriverFactory {
 
     @BeforeAll
     public static void setUp() {
+        Report.configurarExtentReport();
         driver = createBrowser(Browser.CHROME);
         DriverManager.setDriver(driver);
         DriverManager.getDriver();
+        driver.manage().window().maximize();
     }
 
     @AfterAll
     public static void tearDown() {
+        Report.closeReport();
         DriverManager.closeDriver();
     }
 
