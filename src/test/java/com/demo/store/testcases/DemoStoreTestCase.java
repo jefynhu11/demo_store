e8557253ac45d41891c453024fb94bdf09ce8b02
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-16 13:24:55 -0300

    Adicionei import class ProductDemoStore, adicionei metodo para escolhe os dois jogos

--- src/test/java/com/demo/store/testcases/DemoStoreTestCase.java
+++ src/test/java/com/demo/store/testcases/DemoStoreTestCase.java
@@ -2,16 +2,19 @@ package com.demo.store.testcases;
 
 import com.demo.store.frameworks.supports.BaseTestFw;
 import com.demo.store.frameworks.webdrivers.DriverManager;
+import com.demo.store.tasks.ProductDemoStoreTask;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
 public class DemoStoreTestCase extends BaseTestFw {
 
     private final WebDriver driver = DriverManager.getDriver();
+    private final ProductDemoStoreTask productDemoStoreTask = new ProductDemoStoreTask(driver);
 
     @Test
     public void aoRealizarComprasDoisJogos() {
         driver.manage().window().maximize();
         DriverManager.getDriver().get("https://demo.cs-cart.com");
+        productDemoStoreTask.escolheDoisJogos();
     }
 }
