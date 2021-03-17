dc87314d93d55c36e5d2ba2c989e6f86aca1fb48
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-17 10:49:29 -0300

    Adicionei os elementos escolhe, nome, valor e adicionar carrinho por um jogo

--- src/test/java/com/demo/store/appobjects/VideoGamesDemoStoreApp.java
+++ src/test/java/com/demo/store/appobjects/VideoGamesDemoStoreApp.java
@@ -26,8 +26,21 @@ public class VideoGamesDemoStoreApp {
         return driver.findElement(By.id("button_cart_96"));
     }
     public WebElement continuaComprarButton(){
-        Wait.waitVisibilityElement(By.cssSelector("div div div.ty-product-notification__buttons.clearfix div.ty-float-left a"));
-        return driver.findElement(By.cssSelector("div div div.ty-product-notification__buttons.clearfix div.ty-float-left a"));
+        return driver.findElement(By.cssSelector("div > div > div.ty-product-notification__buttons.clearfix > div.ty-float-left > a"));
     }
-
+    public WebElement fear3PS3MXButton(){
+        return driver.findElement(By.id("det_img_90"));
+    }
+    public WebElement fear3PS3MXTextLabel(){
+        return driver.findElement(By.cssSelector("h1 bdi"));
+    }
+    public WebElement fear3PS3MXValueTextLabel(){
+        return driver.findElement(By.id("sec_discounted_price_90"));
+    }
+    public WebElement fear3PS3MXAdicionarAoCarrinhoButton(){
+        return driver.findElement(By.id("button_cart_90"));
+    }
+    public WebElement checkoutButton(){
+        return driver.findElement(By.cssSelector("div > div > div.ty-product-notification__buttons.clearfix > div.ty-float-right > a"));
+    }
 }
