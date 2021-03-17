dbd2dad084359d34a2cb13f3826d1265ce29d676
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-17 09:50:24 -0300

    criei metodo para validar, coloquei js para movimento pra baixo e metodo para fazer acoes clicar

--- src/test/java/com/demo/store/tasks/ProductDemoStoreTask.java
+++ src/test/java/com/demo/store/tasks/ProductDemoStoreTask.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.Status;
 import com.demo.store.appobjects.IndexDemoStoreApp;
 import com.demo.store.appobjects.VideoGamesDemoStoreApp;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 import static com.demo.store.frameworks.tools.Report.extentTest;
@@ -23,6 +24,15 @@ public class ProductDemoStoreTask {
     public void escolheDoisJogos(){
         indexDemoStoreApp.abaVideoGamesButton().click();
         videoGamesDemoStoreApp.batmanArkhamCityX360CEButton().click();
+        validarBatman();
+        ((JavascriptExecutor)driver).executeScript("scroll(0,300)");
+        videoGamesDemoStoreApp.batmanArkhamAdicionarAoCarrinhoButton().click();
+        videoGamesDemoStoreApp.continuaComprarButton().click();
+        indexDemoStoreApp.abaVideoGamesButton().click();
+
+    }
+
+    private void validarBatman() {
         Assertions.assertEquals("Batman: Arkham City (X360) CE", videoGamesDemoStoreApp.batmanArkhamCityX360CETextLabel().getText());
         extentTest.log(Status.PASS, "Validou nome do produto está correto");
         Assertions.assertEquals("99.99", videoGamesDemoStoreApp.batmanArkhamCityValueTextLabel().getText());
