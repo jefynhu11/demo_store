85626efedcf14263dd2de3e380373ffac11865e1
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-17 10:51:59 -0300

    Coloquei Thread só para teste tempo, adicionei acoes e validar um jogo fear

--- src/test/java/com/demo/store/tasks/ProductDemoStoreTask.java
+++ src/test/java/com/demo/store/tasks/ProductDemoStoreTask.java
@@ -21,21 +21,37 @@ public class ProductDemoStoreTask {
         videoGamesDemoStoreApp = new VideoGamesDemoStoreApp(driver);
     }
 
-    public void escolheDoisJogos(){
+    public void escolheDoisJogos() throws InterruptedException {
         indexDemoStoreApp.abaVideoGamesButton().click();
         videoGamesDemoStoreApp.batmanArkhamCityX360CEButton().click();
         validarBatman();
         ((JavascriptExecutor)driver).executeScript("scroll(0,300)");
         videoGamesDemoStoreApp.batmanArkhamAdicionarAoCarrinhoButton().click();
+//        Wait.waitLoadElement(videoGamesDemoStoreApp.continuaComprarButton());
+        Thread.sleep(3000);
         videoGamesDemoStoreApp.continuaComprarButton().click();
-        indexDemoStoreApp.abaVideoGamesButton().click();
 
+        indexDemoStoreApp.abaVideoGamesButton().click();
+        videoGamesDemoStoreApp.fear3PS3MXButton().click();
+        validarFear3();
+        ((JavascriptExecutor)driver).executeScript("scroll(0,300)");
+        videoGamesDemoStoreApp.fear3PS3MXAdicionarAoCarrinhoButton().click();
+        Thread.sleep(3000);
+        videoGamesDemoStoreApp.checkoutButton().click();
     }
 
     private void validarBatman() {
         Assertions.assertEquals("Batman: Arkham City (X360) CE", videoGamesDemoStoreApp.batmanArkhamCityX360CETextLabel().getText());
-        extentTest.log(Status.PASS, "Validou nome do produto está correto");
+        extentTest.log(Status.PASS, "Validou nome do produto está correto: " + videoGamesDemoStoreApp.batmanArkhamCityX360CETextLabel().getText());
         Assertions.assertEquals("99.99", videoGamesDemoStoreApp.batmanArkhamCityValueTextLabel().getText());
-        extentTest.log(Status.PASS, "Validou valor do produto está correto");
+        extentTest.log(Status.PASS, "Validou valor do produto está correto: " + videoGamesDemoStoreApp.batmanArkhamCityValueTextLabel().getText());
+    }
+
+    private void validarFear3() {
+        Assertions.assertEquals("F.E.A.R. 3 (PS3) MX", videoGamesDemoStoreApp.fear3PS3MXTextLabel().getText());
+        extentTest.log(Status.PASS, "Validou nome do produto está correto: " + videoGamesDemoStoreApp.fear3PS3MXTextLabel().getText());
+        Assertions.assertEquals("19.99", videoGamesDemoStoreApp.fear3PS3MXValueTextLabel().getText());
+        extentTest.log(Status.PASS, "Validou valor do produto está correto: " + videoGamesDemoStoreApp.fear3PS3MXValueTextLabel().getText());
     }
 }
+;
