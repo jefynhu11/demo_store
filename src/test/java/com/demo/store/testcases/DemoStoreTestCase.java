073bd66a2db4730cf24022c987d6a23fcd929d2a
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-18 12:21:28 -0300

    Coloquei capture de tela e finalmente de teste para relatorio

--- src/test/java/com/demo/store/testcases/DemoStoreTestCase.java
+++ src/test/java/com/demo/store/testcases/DemoStoreTestCase.java
@@ -3,6 +3,7 @@ package com.demo.store.testcases;
 import com.aventstack.extentreports.Status;
 import com.demo.store.frameworks.supports.BaseTestFw;
 import com.demo.store.frameworks.tools.Report;
+import com.demo.store.frameworks.tools.Screenshot;
 import com.demo.store.frameworks.webdrivers.DriverManager;
 import com.demo.store.tasks.CheckoutDemoStoreTask;
 import com.demo.store.tasks.ProductDemoStoreTask;
@@ -18,11 +19,12 @@ public class DemoStoreTestCase extends BaseTestFw {
     private final CheckoutDemoStoreTask checkoutDemoStoreTask = new CheckoutDemoStoreTask(driver);
 
     @Test
-    public void aoRealizarComprasDoisJogos() throws InterruptedException {
+    public void aoRealizarComprasDoisJogos() {
         Report.createTest("Ao realizar comprar os dois jogos");
         DriverManager.getDriver().get("https://demo.cs-cart.com");
-        extentTest.log(Status.INFO, "Abriu a navegador no DEMO STORE");
+        extentTest.log(Status.INFO, "Abriu a navegador no DEMO STORE", Screenshot.screenshotBase64(driver));
         productDemoStoreTask.escolheDoisJogos();
         checkoutDemoStoreTask.registrandoComprando();
+        extentTest.log(Status.INFO, "Finalmente de teste", Screenshot.screenshotBase64(driver));
     }
 }
